e1beadc95e4073d60349c09f1a789d75a65e9372
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2014-05-08 11:44:20 +1000

    Improve the accuracy of the pixels drawn

--- Prototypes/DrawMouse/CanvasDraw.js
+++ Prototypes/DrawMouse/CanvasDraw.js
@@ -3,7 +3,11 @@ var ctx = el.getContext('2d');
 
 ctx.fillStyle = 'red';
 
+ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+
+  
 el.onmousemove = function(e) {  
-  ctx.fillRect(e.clientX - 5, e.clientY, 1, 1);
+  ctx.fillRect(e.clientX - 8, e.clientY - 8, 1, 1);
   
 };
