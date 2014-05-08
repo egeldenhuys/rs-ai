bb4bd375f03b4b07e28b820f2f78d59fdceeefaa
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2014-05-08 12:20:05 +1000

    Stuff

--- Prototypes/DrawMouse/CanvasDraw.js
+++ Prototypes/DrawMouse/CanvasDraw.js
@@ -1,13 +1,10 @@
 var el = document.getElementById('c');
 var ctx = el.getContext('2d');
 
+ctx.canvas.width  = window.innerWidth - 20;
+ctx.canvas.height = window.innerHeight - 20;
 ctx.fillStyle = 'red';
-
-ctx.canvas.width  = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
-
   
 el.onmousemove = function(e) {  
-  ctx.fillRect(e.clientX - 8, e.clientY - 8, 1, 1);
-  
+  ctx.fillRect(e.clientX - 8, e.clientY - 8, 2, 2);
 };
